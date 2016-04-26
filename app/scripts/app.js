eabac16bb2f939e4a296c6ee0730398095455f97
Oj Obasi <ericel123new@gmail.com>
2016-04-27 08:59:29 +0900

    Displaying cvs array data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,36 +36,178 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     console.log('Our app is ready to rock!');
   });
 
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
 
   // See https://github.com/Polymer/polymer/issues/1381
 window.addEventListener('WebComponentsReady', function() {
+     var tripResults = document.querySelector('#results');
+    // Get Train Stops to datalist
+    get('../data/stops.txt').then(function(response) {
+      //console.log("Success!", response.split(/(\r\n|\n)/));
+      var stop_list = response.split(/(\r\n|\n)/);
+      var re = /,/;
+      var headers = stop_list.shift().split(re);
+      var index = headers.indexOf("stop_name");
+      //Removing [index] at return val.split(re)[index]; should return an array of arrays of each split value
+      var res = stop_list.map(function(val, key) {
+        return val.split(re)[index];
+      }).filter(Boolean);
 
-function converttxttoArray(filename) {
-  var reader = (window.XMLHttpRequest != null) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-  reader.open("GET", filename, false);
-  reader.send();
-  return reader.responseText.split(/(\r\n|\n)/);
-}
-// Get Train Stops to datalist
-    var stop_list = converttxttoArray("../data/stops.txt");
-    var re = /,/;
-    var headers = stop_list.shift().split(re);
-    var index = headers.indexOf("stop_name");
-    var res = stop_list.map(function(val, key) {
-      return val.split(re)[index];
-    }).filter(Boolean);
-    console.log(res);
-    //var stopArray = res.split(',');
-    var str = '';
-    var i;
-    for (i = 0; i < res.length; i++) {
-       str += '<option value="'+res[i]+'" />';
+       var str = '';
+        var i;
+        for (i = 0; i < res.length; i++) {
+           str += '<option value="'+res[i]+'" />';
+        }
+        var s_list=document.getElementById("slist");
+        s_list.innerHTML = str;
+    }, function(error) {
+      console.error("Failed!", error);
+    });
+
+    /*get('../data/trips.txt').then(function(response) {
+      //console.log("Success!", response.split(/(\r\n|\n)/));
+      var trips_list = response.split(/(\r\n|\n)/);
+      if(Array.isArray(trips_list)) {
+        console.log('true');
+      } else {
+        console.log('Not An Array')
+      }
+      var re = /,/;
+      var headers = trips_list.shift().split(re);
+      var route_id = headers.indexOf("route_id");
+      var service_id = headers.indexOf("service_id");
+      var trip_id = headers.indexOf("trip_id");
+      var trip_headsign = headers.indexOf("trip_headsign");
+      var mainArray = trips_list.map(function(val, key) {
+        return val.split(re);
+      }).filter(Boolean);
+    
+      var trip_id = headers.indexOf("trip_id");
+      var tripid = trips_list.map(function(val, key) {
+        return val.split(re)[trip_id];
+      }).filter(Boolean);
+
+       var service_id = headers.indexOf("service_id");
+      var serviceid = trips_list.map(function(val, key) {
+        return val.split(re)[service_id];
+      }).filter(Boolean);
+
+      var route_id = headers.indexOf("route_id");
+      var routeid = trips_list.map(function(val, key) {
+        return val.split(re)[route_id];
+      }).filter(Boolean);
+
+
+       var tripstr = '';
+        var i;
+        for (i = 0; i < mainArray.length; i++) {
+           tripstr += '<div class="tripper"><b>RID:</b> <em>'+routeid[i]+'</div>';
+        }
+
+        tripResults.innerHTML = tripstr;
+        //console.log(tripstr);
+    }, function(error) {
+      console.error("Failed!", error);
+    });
+ */
+
+
+   function doStuff(data) {
+      var tripstr = '';
+        //Data is usable her
+       // console.log(data);
+       for (var prop in data) {
+        if(data.hasOwnProperty( prop ) ) {
+          console.log("obj." + prop + " = " + data[prop]);
+          console.log(data.route_id);
+        } 
+      }
+
+      
+
+      
+            tripResults.innerHTML = tripstr;
     }
-    var s_list=document.getElementById("slist");
-    s_list.innerHTML = str;
-  
+
+    function parseData(url, callBack) {
+        Papa.parse(url, {
+            download: true,
+            dynamicTyping: true,
+            header: true,
+            complete: function(results) {
+                callBack(results.data);
+            }
+        });
+    }
+
+    parseData("../data/routes.txt", doStuff);
+ 
+    
 
 
+   //IndexedDB
+   var dbPromise = idb.open('tester', 1, function(upgradeDb) {
+    var keyValStore = upgradeDb.createObjectStore('keyval');
+    keyValStore.put(28, 'age');
+   });
+   
+   //read IndexedDb
+   dbPromise.then(function(db) {
+    var tx = db.transaction('keyval');
+    var keyValStore = tx.objectStore('keyval');
+    return keyValStore.get('age');
+   }).then( function(val) {
+    console.log('The value of "age" is: ', val);
+   });
+
+   // Add another value
+   dbPromise.then(function(db) {
+    var tx = db.transaction('keyval', 'readwrite');
+    var keyValStore = tx.objectStore('keyval');
+    keyValStore.put('Obasi', 'Name');
+    return tx.complete;
+   }).then(function() {
+    console.log('Added name:Obasi to keyval');
+   });
+
+   dbPromise.then(function(db) {
+    var tx = db.transaction('keyval', 'readwrite');
+    var keyValStore = tx.objectStore('keyval');
+    keyValStore.put('dog', 'favoriteAnimal');
+    return keyValStore.get('favoriteAnimal');
+   }).then(function(val) {
+     console.log('Favorite Animal is:', val);
+   })
+ 
 });
 
 
